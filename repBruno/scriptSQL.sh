09566f29220e7a7ff72858dfd8710b63b2860c31
Bruno Henrique <brunoo_henriquee_chagas@hotmail.com>
2022-05-30 21:06:42 -0300

    arquivo docker

---
+++ repBruno/scriptSQL.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+docker --version
+
+if [ $? -eq 0 ]
+
+then
+
+echo "docker instalado"
+
+else
+
+echo "docker não instalado, instalando o docker"
+
+sudo apt install docker.io
+
+echo "Startando o docker"
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+echo "Subindo imagem MySql Docker"
+
+sudo docker pull: mysql 5.7
+
+fi
+
+echo "deseja criar o seu banco dentro do docker?\n
+
+s|n"
+
+read resposta
+
+if [ "$resposta" == "s" ]
+
+then
+
+        echo "criando o MySql, qual o nome do seu container?"
+
+        read nome
+
+        echo " qual o nome do seu banco de dados?"
+
+        read banco
+
+        echo "qual a senha do seu banco de dados?"
+
+        read senha
+
+
+
+        sudo docker run -d -p 3306:3306 --name $nome -e "MYSQL_DATABASE=$banco" -e "MYSQL_ROOT_PASSWORD=$senha" mysql:5.7
+
+fi
+
+
+
+echo "Deseja executar o MySql?\n s|n"
+
+read resposta2
+
+if [ "$resposta2" == "s" ]
+
+then
+
+        sudo docker ps -a
+
+        echo "digite o nome do seu container"
+
+        read nome
+
+
+
+        echo "ao entrar de o comando mysql -u root -p e coloquei a senha do seu usuario para entrar no banco de dados"
+
+sudo docker exec -it $nome bash
+
+echo "$id"
+
+fi
